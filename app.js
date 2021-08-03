9a696827eda2d98845ac9b344b8a86d07ee53cdc
Paul_D_Edward <edward2000ed@gmail.com>
2021-08-03 08:08:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ dbConnect.then(() => {
     sendMailTimer = setInterval(() => {
         const thisDay = new Date();
         console.log("set interval executed")
-        if (thisDay.getDay() == dayToSend) {
+        if (thisDay.getDay() == dayToSend[0] || thisDay.getDay() == dayToSend[1]) {
             calculateTiming();
         }
     }, process.env.INTERVAL_TIME);
